37d4a4791d7e0f96cfce1d4b12b67221ccfd13b2
PedroHLC ☭ <root@pedrohlc.com>
2023-11-08 15:41:22 -0300

    builder: add text-loader for SVGs

--- builder/src/common.ts
+++ builder/src/common.ts
@@ -1,12 +1,13 @@
 import EnvPlugin, { getGitDescribe } from './env-plugin';
 import PrepareHtmlPlugin from './prepare-html-plugin';
 import ElmPlugin from 'esbuild-plugin-elm';
+import { Loader } from 'esbuild';
 
 const projectPath = process.env.DEVENV_ROOT ?? process.cwd();
-const _gitDescribe = getGitDescribe();
-
-// Update it here and "src/env.ts"
+const _gitDescribe = getGitDescribe(); // Adds it (when missing) to GIT_DESCRIBE
 const environment = process.env.APP_ENV ?? 'development';
+
+const loader: { [ext: string]: Loader } = { '.svg': 'text' };
 
 const options = {
   target: ['es2021'],
@@ -18,6 +19,7 @@ const options = {
   metafile: true,
   minify: environment === 'production',
   sourcemap: true,
+  loader,
   plugins: [
     EnvPlugin(),
     ElmPlugin({ pathToElm: 'env-elmlvl2-wrapper' }),
